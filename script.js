e3ee7ecf8a5c94a427d6ed4b06b34dbdc2caa260
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-10 12:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace("#", "");
+
+  // Split the short hex code into color components
+  const red = shortHex[0];
+  const green = shortHex[1];
+  const blue = shortHex[2];
+
+  // Create the full hex code by repeating each color component
+  const fullHex = `#${red}${red}${green}${green}${blue}${blue}`;
+
+  // Return the full hex code
+  return fullHex;
+	
 };
 
 // Do not change the code below.
